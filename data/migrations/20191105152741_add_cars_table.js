3857020c2c056ed3dac95c4d4ccfaed9dcbf552a
Jasmine <jasminecharles@hotmail.com>
2019-11-05 15:50:47 -0800

    set-up

---
+++ data/migrations/20191105152741_add_cars_table.js
@@ -0,0 +1,18 @@
+//the changes to make
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', function (table) {
+//adds a primary key called 'id' as an auto-incrementing integer, not null and unique
+        table.increments();
+        table.string('vin', 128).notNullable();
+        table.string('make', 64).notNullable();
+        table.string('model', 64).notNullable();
+        table.float('mileage').notNullable();
+        table.string('transmission', 64);
+        table.string('title', 128);
+        table.timestamps();
+      }) 
+};
+//how to undo those changes
+exports.down = function(knex) {
+ return knex.schema.dropTableIfExists('cars') 
+};
